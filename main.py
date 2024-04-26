adca30a187b90ac168d2034895700e593a7ecc5c
Wyatt5150 <76788434+Wyatt5150@users.noreply.github.com>
2024-04-26 14:14:31 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,52 @@
 import csv
 import glob
+import pandas as pd
 
-# Note - due to most people having two put files
-# The activity keys are Put1 = 3 and Put2 = 31 
-# So Put2 would have to be manually adjusted at the end
-Activities = {
-    "Data\\Drink.csv" : "A01",
-    "Data\\Pick.csv" : "A02",
-    "Data\\Put1.csv" : "A03",
-    "Data\\Put2.csv" : "A031",
-    "Data\\Sweep.csv" : "A05",
-    "Data\\Wash.csv" : "A06",
-    "Data\\Wave.csv" : "A07",
-    "Data\\Walk.csv" : "A08",
-    "Data\\Sit.csv" : "A09",
-    "Data\\Back.csv" : "A10",
-    "Data\\Front.csv" : "A11",
-    "Data\\Left.csv" : "A12",
-    "Data\\Right.csv" : "A13",
-    "Data\\Rotate.csv" : "A14"
+Subjects = {
+    "Subject_Name_Here" : "SID_Number_Here"
 }
 
-# Takes in all csv files from data
-csvFileNames = glob.glob("Data/*.csv")
-print(csvFileNames)
+Activities = {
+    "Drink" : "01",
+    "Pick" : "02",
+    "Put" : "03",
+    "Sweep" : "05",
+    "Wash" : "06",
+    "Wave" : "07",
+    "Walk" : "08",
+    "Sit" : "09",
+    "Back" : "10",
+    "Front" : "11",
+    "Left" : "12",
+    "Right" : "13",
+    "Rotate" : "14"
+}
 
-# LOAD ID HERE, S--
-subject = 'S39' 
 
-for file in csvFileNames:
-    # Checks for activity in Activities map, prints if activity is missed
-    if not (file in Activities.keys()):
-        print(file, "- missed")
-        continue
-    prefix = ".\\Final\\" + subject + Activities[file] + 'T0'
+def FindId(query, document):
+    id = -1
+    for subject in document:
+        if subject.lower() in query.lower():
+            id = str(document[subject])
+            break
+    return id
 
+def Get_Page(filePath):
+    file = pd.ExcelFile(filePath)
+    page = -1
+    for sheet in file.sheet_names:
+        if "trim" in sheet:
+            page = sheet
+            break
+
+    return page
+
+def Sort_CSV(src, dst):
     data = []
-
     # Stores each important line from the file and stores it in a temp array
     # Then appends that temp array to data
     # Determines cuttof by blank spaces and skips the very first line
-    with open(file, 'r') as ReadFile:
+    with open(src, 'r') as ReadFile:
         reader = csv.reader(ReadFile, delimiter=',')
         
         temp_data = []
@@ -49,6 +55,8 @@ for file in csvFileNames:
         for row in reader:
             if not accepting:
                 accepting = True
+                continue
+            if len(row) < 1:
                 continue
             if row[0] == '':
                 if len(temp_data) > 0:
@@ -59,12 +67,46 @@ for file in csvFileNames:
         if len(temp_data) > 0:
             data.append(temp_data)
 
-
     # Dumps each array from data into it's own csv file
     for i in range(0, len(data)):
-        toWrite = prefix + str(i+1) + ".csv"
+        toWrite = dst + str(i+1) + ".csv"
         with open(toWrite, 'w', newline='') as csvfile:
             writer = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL)
             for line in data[i]:
                 writer.writerow(line)
     
+
+folderNames = glob.glob("Data/*")
+
+for folder in folderNames:
+    SID = FindId(folder, Subjects)
+    if SID == -1:
+        print("Could not find SID associated with: ", folder)
+        continue
+    SID = "S" + SID
+
+    fileNames = glob.glob(folder + "/*.xlsx")
+    for file in fileNames:
+        AID = FindId(file, Activities)
+        if AID == -1:
+            print("Could not find AID associated with: ", file)
+            continue
+        AID = "A" + AID
+
+        page = Get_Page(file)
+        if page == -1:
+            print("Could not parse file: ", file)
+            continue
+
+        sheet = pd.read_excel(file, sheet_name = page, header = 0)
+
+        csv_loc = {
+            'p_a' : [f'CSVs/P_Accel_Raw/{SID + AID}.csv', f'CSVs/P_Accel_Final/{SID + AID}T0'],
+            'p_g' : [f'CSVs/P_Gyro_Raw/{SID + AID}.csv', f'CSVs/P_Gyro_Final/{SID + AID}T0'],
+            'w_a' : [f'CSVs/W_Accel_Raw/{SID + AID}.csv', f'CSVs/W_Accel_Final/{SID + AID}T0'],
+            'w_g' : [f'CSVs/W_Gyro_Raw/{SID + AID}.csv', f'CSVs/W_Gyro_Final/{SID + AID}T0']
+        }
+
+        for key in csv_loc:
+            sheet.to_csv(csv_loc[key][0], encoding='utf-8', index = False, columns=sheet.filter(like=key))
+            Sort_CSV(csv_loc[key][0],csv_loc[key][1])
